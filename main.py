df9829a4195c315a8e7d4096e6714ba1014a2d99
Joseph Miele <josephmiele3@gmail.com>
2024-04-04 20:04:26 -0400

    increasing difficulty as the floor increases

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="DANGAR DUNJON",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
